6c14bd94b6171ba9586afefda885b517b96c8669
bryanm-lagash <bryanm@lagash.com>
2019-11-28 19:34:57 -0300

    Se realizan cambios en view CrearContrato

---
+++ SLN_Calidad/App/Models/ViewModels/FinalContratoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Models.ViewModels
+{
+    public class FinalContratoViewModel
+    {
+        //Datos Contrato
+        public int ContratoId { get; set; }
+        public System.DateTime FechaCreacion { get; set; }
+        public System.DateTime FechaInicio { get; set; }
+        public System.DateTime FechaTermino { get; set; }
+        public int EmpleadoId { get; set; }
+
+        //Valor Hora
+        public int ValorHoraId { get; set; }
+        public string Tipo { get; set; }
+        public double Valor { get; set; }
+
+        //Descuentos
+
+        public int DescuentoId { get; set; }
+        public Nullable<double> Salud { get; set; }
+        public Nullable<double> AFP { get; set; }
+        public Nullable<double> Otros { get; set; }
+
+
+    }
+}
