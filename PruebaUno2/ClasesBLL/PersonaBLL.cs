e7b745d040f126ddd9e7651b38834577e897f3dd
Abraham <antonio.vidal95@hotmail.com>
2019-12-02 21:06:04 -0300

    prueba2

---
+++ PruebaUno2/ClasesBLL/PersonaBLL.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesDAL;
+using System.Data.SqlClient;
+
+
+namespace ClasesBLL
+{
+   public class PersonaBLL
+    {
+       
+        public void Crear()
+        {
+            PruebaOneEntities modelo = new PruebaOneEntities();
+
+            Persona buscado = modelo.Persona.Where(c => c.Rut == Rut).FirstOrDefault();
+
+            if (buscado == null)
+            {
+                Persona p = new Persona();
+                p.Rut = Rut;
+                p.Nombre = Nombre;
+                p.Apellidos = Apellidos;
+                p.Genero = Genero;
+                p.Salario = Salario;
+                p.FechaNacimiento = FechaNacimiento;
+
+                modelo.Persona.Add(p);
+                modelo.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Persona ya existe");
+            }
+        }
+
+
+
+
+
+
+
+
+        private string rut;
+        public string Rut {
+            get
+            {
+                return rut;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) {
+                    //if (Herramientas.validarRut(value))
+                    //{
+                    //    rut = value;
+                    //}
+                    //else
+                    //{
+                    //    throw new Exception("El rut es invalido");
+                    //}
+                    rut = value;
+                }
+                else
+                {
+                    throw new Exception("El rut no puede estar vacio");
+                }
+            }
+        }
+
+        private string nombre;
+        public string Nombre {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value.ToString()))
+                {
+                    nombre = value;
+                }
+                else
+                {
+                    throw new Exception("Nombre no puede estar vacio");
+                }
+            }
+        }
+
+        private string apellidos;
+        public string Apellidos {
+            get
+            {
+                return apellidos;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value.ToString()))
+                {
+                    apellidos = value;
+                }
+                else
+                {
+                    throw new Exception("Apellido no puede estar vacio");
+                }
+            }
+        }
+
+        private string genero;
+        public string Genero {
+            get
+            {
+                return genero;
+            }
+            set
+            {
+                if(!string.IsNullOrEmpty(value.ToString()))
+                {
+                    genero = value;
+                }
+                else
+                {
+                    throw new Exception("Genero no puede estar vacio");
+                }
+            }
+          
+        }
+        public int Salario { get; set; }
+        public Nullable<System.DateTime> FechaNacimiento { get; set; }
+    }
+}
---
+++ PruebaUno2/PruebaUno/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClasesBLL;
+
+namespace PruebaUno
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PersonaBLL p = new PersonaBLL();
+            p.Rut = txtRut.ToString();
+            p.Nombre = txtNombre.ToString();
+            p.Apellidos = txtApellido.ToString();
+            p.Genero = txtGenero.ToString();
+            p.Salario = int.Parse(txtSalario.Text);
+            DateTime fecha = Convert.ToDateTime(string.Format("{0:yyyy-MM-dd}", txtFechaNacimiento));
+            p.FechaNacimiento = fecha;
+
+            p.Crear();
+        }
+    }
+}
